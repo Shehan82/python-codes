355bc5edf6fd929f62e570ebb52eef9841f60b88
Shehan82 <Shehansandeepa82@.github.com>
2019-10-17 23:34:05 +0530

    important python code

---
+++ loops_important.py
@@ -0,0 +1,4 @@
+
+presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson"]
+for i in range(len(presidents)):
+    print("President {}: {}".format(i + 1, presidents[i]))
