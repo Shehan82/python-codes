b93f694f8021e4e5d6f7f64f25bdab2bf6dd0241
Shehan82 <Shehansandeepa82@.github.com>
2019-10-17 23:37:42 +0530

    array and function

---
+++ array.py
@@ -0,0 +1,12 @@
+import array
+print("Enter 5 numbers")
+arr=array.array("i",[])
+for i in range(0,5):
+    x=int(input())
+    arr.append(x)
+for i in range(0,len(arr)):
+    print(arr[i],end=' ')
+
+list=[1,2,3,4]
+for i in range(len(list)):
+    print(list[i])
---
+++ function.py
@@ -0,0 +1,10 @@
+from math import *
+
+def printw():
+    name=input("Enter your name: ")
+    if(name=="shehan"):
+        print(name)
+    else:
+        print("wrong name")
+
+printw()
