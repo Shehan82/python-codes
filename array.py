826e04d647b75c0feb4fa8afb7c042fe1daf3cd7
shehan sandeepa <shehansandeepa82@gmail.com>
2019-10-19 23:17:59 +0530

    remove print line

--- array.py
+++ array.py
@@ -10,10 +10,3 @@ for i in range(0,len(arr)):
 list=[1,2,3,4]
 for i in range(len(list)):
     print(list[i])
-<<<<<<< HEAD
-print("shehan")
-print("booooooooooooo")
-=======
-    
-print("shehan")
->>>>>>> 9d1bff2993163db3f9ae2d5ca289abc8346203ee
