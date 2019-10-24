722fd5ec50d0a1d0680cae6bf51383da03ee269f
shehan sandeepa <shehansandeepa82@gmail.com>
2019-10-24 22:54:18 +0530

    append([a,b,c])

--- python list hckr.py
+++ python list hckr.py
@@ -2,14 +2,11 @@ x = int(input())
 y = int(input())
 z = int(input())
 n = int(input())
-count=-1
+count=0
 large_list=[]
 for i in range(x + 1):
     for j in range( y + 1):
         for k in range(z+1):
             if(i+j+k!=n):
-                count=count+1
-                print(i,j,k)
-                print(count)
-                #large_list[count].append(i,j,k)
-#print(large_list)
+                large_list.append([i,j,k])
+print(large_list)
